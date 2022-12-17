f0c5c7d57e450dc05ef7c6b3a0498070e9d1fd85
HiboCalasow <hibo6912@gmail.com>
2022-12-17 07:36:14 -0800

    login page and logout

---
+++ auth.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+if($_SESSION['status']!=1){
+    ?>
+    <script>
+        alert("you are allowed the system first login");
+        window.location="index.php";
+    </script>
+    <?php
+}
+?>
---
+++ logout.php
@@ -0,0 +1,5 @@
+<?php
+session_destroy();
+$_SESSION['status']=0;
+header("location:index.php");
+?>
